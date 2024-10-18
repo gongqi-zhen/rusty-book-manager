9b8f8c1e6761a6b88c84724726728422dc854eee
gongqi.zhen <gongqi.zhen@gmail.com>
2024-10-18 12:17:41 +0900

    P52. add simple unit test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::{Ipv4Addr, SocketAddr};
 use anyhow::Result;
 use axum::{http::StatusCode, routing::get, Router};
+use std::net::{Ipv4Addr, SocketAddr};
 use tokio::net::TcpListener;
 
 pub async fn health_check() -> StatusCode {
@@ -15,4 +15,10 @@ async fn main() -> Result<()> {
 
     println!("Listening on {}", addr);
     Ok(axum::serve(listener, app).await?)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
 }
