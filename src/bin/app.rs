ceb6a389054e299c03d191764ea59fed417ace9a
gongqi.zhen <gongqi.zhen@gmail.com>
2024-11-07 11:58:24 +0900

    P272 - P277 OpenTelemetry with JAEGER https://github.com/jaegertracing/jaeger

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -2,6 +2,7 @@ use adapter::{database::connect_database_with, redis::RedisClient};
 use anyhow::{Context, Result};
 use api::route::{auth, v1};
 use axum::{http::Method, Router};
+use opentelemetry::global;
 use registry::AppRegistryImpl;
 use shared::config::AppConfig;
 use shared::env::{which, Environment};
@@ -30,6 +31,20 @@ fn init_logger() -> Result<()> {
         Environment::Production => "info",
     };
 
+    let host = std::env::var("JAEGER_HOST")?;
+    let port = std::env::var("JAEGER_PORT")?;
+    let endpoint = format!("{host}:{port}");
+
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_endpoint(endpoint)
+        .with_service_name("book-manager")
+        .with_auto_split_batch(true)
+        .with_max_packet_size(8192)
+        .install_simple()?;
+
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
 
     let subscriber = tracing_subscriber::fmt::layer()
@@ -43,6 +58,7 @@ fn init_logger() -> Result<()> {
     tracing_subscriber::registry()
         .with(subscriber)
         .with(env_filter)
+        .with(opentelemetry)
         .try_init()?;
 
     Ok(())
@@ -77,6 +93,7 @@ async fn bootstrap() -> Result<()> {
     tracing::info!("Listening on {}", addr);
 
     axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .context("Unexpected error happened in server")
         .inspect_err(|e| {
@@ -93,4 +110,38 @@ fn cors() -> CorsLayer {
         .allow_headers(cors::Any)
         .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
         .allow_origin(cors::Any)
+}
+
+async fn shutdown_signal() {
+    fn purge_spans() {
+        global::shutdown_tracer_provider();
+    }
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install SIGTERM signal handler")
+            .recv()
+            .await
+            .expect("Failed to receive SIGTERM signal");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            tracing::info!("Ctrl-Cを受信しました。");
+            purge_spans()
+        },
+        _ = terminate => {
+            tracing::info!("SIGTERMを受信しました。");
+            purge_spans()
+        }
+    }
 }
